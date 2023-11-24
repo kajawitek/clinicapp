fd9898e19754dbbe7619c23400eb5dc3aa244273
Kaja Witek <kaja.wasiolek@gmail.com>
2023-11-24 12:25:10 +0100

    feat: add pagination with pagy gem

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'rubocop-rails'
 gem 'sprockets-rails'
 gem 'stimulus-rails'
 gem 'turbo-rails'
+gem 'pagy'
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class PatientsController < ApplicationController
+
   def index
     q = Patient.ransack(params[:q])
-    patients = q.result(distinct: true)
-    render :index, locals: { q: q, patients: patients }
+    items_amount = 5
+    pagy, patients = pagy(q.result(distinct: true), items: items_amount)
+    render :index, locals: { q: q, patients: patients, pagy: pagy, items_amount: items_amount }
   end
 end
