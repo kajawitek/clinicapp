80a7edacf65d504e468c794e85cbf85631b66170
Kaja Witek <kaja.wasiolek@gmail.com>
2023-11-27 09:06:36 +0100

    refactor: add gender enum to patient model, update sorting links

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,6 +4,11 @@ class Patient < ApplicationRecord
   validates :first_name, :last_name, :pesel, :gender, :date_of_birth, :city, presence: true
   validates :pesel, uniqueness: true
 
+  enum gender: {
+    male: 'male',
+    female: 'female'
+  }
+
   def self.ransackable_attributes(_auth_object = nil)
     %w[last_name date_of_birth gender]
   end
