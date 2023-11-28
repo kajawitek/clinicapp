e8788ec471fff605c6437ea9e5a285d867167556
Kaja Witek <kaja.wasiolek@gmail.com>
2023-11-28 13:56:05 +0100

    feat: add a doctor model and a unit spec to it

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_134830) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_113034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name", null: false
