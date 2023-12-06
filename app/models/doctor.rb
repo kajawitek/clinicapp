7bae7936f074405f767fb4023f2366433508c615
Kaja Witek <kaja.wasiolek@gmail.com>
2023-12-06 21:32:40 +0100

    feat: add full_name method to doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,4 +4,8 @@ class Doctor < ApplicationRecord
   has_many :appointment_slots, dependent: :destroy
 
   validates :first_name, :last_name, presence: true
+
+  def full_name
+    [first_name, last_name].join(' ')
+  end
 end
