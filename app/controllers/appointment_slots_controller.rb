0b89db252241dc41465f043cbde4624cde42a4af
Kaja Witek <kaja.wasiolek@gmail.com>
2023-12-07 23:49:13 +0100

    feat: add booking appointments

--- app/controllers/appointment_slots_controller.rb
+++ app/controllers/appointment_slots_controller.rb
@@ -5,12 +5,18 @@ class AppointmentSlotsController < ApplicationController
     q = AppointmentSlot.ransack(params[:q])
     items_amount = 10
     pagy, appointment_slots = pagy(q.result(distinct: true).includes(:doctor), items: items_amount)
-    doctors = Doctor.joins(:appointment_slots).distinct.map { |doctor| [doctor.full_name, doctor.id] }
 
-    render :index, locals: { q: q,
-                             appointment_slots: appointment_slots,
-                             pagy: pagy,
-                             items_amount: items_amount,
-                             doctors: doctors }
+    render :index, locals: { q: q, appointment_slots: appointment_slots, pagy: pagy, items_amount: items_amount,
+                             doctors: doctors, patient: patient }
+  end
+
+  private
+
+  def doctors
+    Doctor.joins(:appointment_slots).distinct.map { |doctor| [doctor.full_name, doctor.id] }
+  end
+
+  def patient
+    Patient.find(params[:patient]) if params[:patient]
   end
 end
---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class AppointmentsController < ApplicationController
+  def new
+    patient = Patient.find(params[:patient])
+    appointment = Appointment.new(patient: patient)
+    slot = AppointmentSlot.find(params[:slot])
+    render :new, locals: { appointment: appointment, patient: patient, slot: slot }
+  end
+
+  def create
+    appointment = Appointment.new(appointment_params)
+    if appointment.save
+      redirect_to root_path, notice: 'Appointment created successfully!'
+    else
+      patient = Patient.find(appointment_params[:patient_id])
+      slot = AppointmentSlot.find(appointment_params[:appointment_slot_id])
+      flash.now[:alert] = 'Appointment not created!'
+      render :new, locals: { appointment: appointment, patient: patient, slot: slot }
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:patient_id, :price, :appointment_slot_id)
+  end
+end
