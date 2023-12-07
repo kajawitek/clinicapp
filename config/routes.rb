fffd1fd26893a30e8d10f6206960ce4ff5d9aba2
Kaja Witek <kaja.wasiolek@gmail.com>
2023-12-07 23:38:28 +0100

    feat: add appointment to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'patients#index'
 
   resources :patients, only: %i[index]
+  resources :appointments, only: %i[new create]
   resources :appointment_slots, only: %i[index]
 end
