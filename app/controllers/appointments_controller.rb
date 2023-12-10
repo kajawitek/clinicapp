810849ae141ca9519802931d7d224a669081e230
Kaja Witek <kaja.wasiolek@gmail.com>
2023-12-10 10:30:33 +0100

    style: appointments_controller.rb

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,17 +5,20 @@ class AppointmentsController < ApplicationController
     patient = Patient.find(params[:patient])
     appointment = Appointment.new(patient: patient)
     slot = AppointmentSlot.find(params[:slot])
+
     render :new, locals: { appointment: appointment, patient: patient, slot: slot }
   end
 
   def create
     appointment = Appointment.new(appointment_params)
+
     if appointment.save
-      redirect_to root_path, notice: 'Appointment created successfully!'
+      redirect_to patients_path, notice: 'Appointment created successfully!'
     else
       patient = Patient.find(appointment_params[:patient_id])
       slot = AppointmentSlot.find(appointment_params[:appointment_slot_id])
       flash.now[:alert] = 'Appointment not created!'
+
       render :new, locals: { appointment: appointment, patient: patient, slot: slot }
     end
   end
