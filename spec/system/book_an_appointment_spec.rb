318324912fb90cbd9ec961270ffacf5ca679d37e
Kaja Witek <kaja.wasiolek@gmail.com>
2023-12-14 11:07:34 +0100

    fix: fix booking appointment after filter doctor, update spec

--- spec/system/book_an_appointment_spec.rb
+++ spec/system/book_an_appointment_spec.rb
@@ -23,4 +23,35 @@ RSpec.describe 'Booking an appointment' do
     click_button 'Create Appointment'
     expect(page).to have_content('Appointment created successfully!')
   end
+
+  it 'books appointment when valid, and return error notice when invalid with filtering a patient and a doctor' do
+    create(:patient, first_name: 'John', last_name: 'Doe')
+    appointment_slot = create(:appointment_slot, doctor: create(:doctor, first_name: 'Adam', last_name: 'Noe'))
+    create(:appointment_slot, doctor: create(:doctor, first_name: 'Donald', last_name: 'Draper'))
+
+    visit root_path
+
+    click_link 'Patients'
+    click_button 'Search'
+
+    click_link 'Add appointment'
+    select 'Adam Noe', from: 'q[doctor_id_eq]'
+    click_button 'Search'
+    within('#appointment-slots-table') do
+      expect(page).to have_content('Adam Noe')
+      expect(page).not_to have_content('Donald Draper')
+    end
+
+    click_link 'Book appointment'
+    expect(page).to have_content(appointment_slot.doctor.full_name)
+    expect(page).to have_content(appointment_slot.date_and_time)
+
+    click_button 'Create Appointment'
+    expect(page).to have_content('Appointment not created!')
+    expect(page).to have_content("Pricecan't be blank")
+
+    fill_in 'appointment_price', with: 12
+    click_button 'Create Appointment'
+    expect(page).to have_content('Appointment created successfully!')
+  end
 end
