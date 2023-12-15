4f5aaad245a0700ce60a38a2eca3d056a68134fe
Kaja Witek <kaja.wasiolek@gmail.com>
2023-12-15 10:49:35 +0100

    style: correct typo

--- app/controllers/appointment_slots_controller.rb
+++ app/controllers/appointment_slots_controller.rb
@@ -4,10 +4,10 @@ class AppointmentSlotsController < ApplicationController
   def index
     q = AppointmentSlot.ransack(params[:q])
     items_amount = 10
-    pagy, avaiable_appointment_slots = pagy(q.result(distinct: true).where(available: true).includes(:doctor),
+    pagy, available_appointment_slots = pagy(q.result(distinct: true).where(available: true).includes(:doctor),
                                             items: items_amount)
 
-    render :index, locals: { q: q, avaiable_appointment_slots: avaiable_appointment_slots, pagy: pagy,
+    render :index, locals: { q: q, available_appointment_slots: available_appointment_slots, pagy: pagy,
                              items_amount: items_amount, doctors: Doctor.doctors_for_select, patient: patient }
   end
 
